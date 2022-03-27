632cd61b557861b2514ee05708affed36fbc7c62
alexzhang1030 <1642114555@qq.com>
2022-03-27 16:48:50 +0800

    docs: lint style

--- src/components/FeaturesList.vue
+++ src/components/FeaturesList.vue
@@ -8,12 +8,12 @@
     <ListItem>使用你的应用程序中的相同设置来进行测试！</ListItem>
     <ListItem><a href="https://twitter.com/antfu7/status/1468233216939245579" target="_blank">智能检测和即时观察模式，就像是测试中的 HMR 一样！</a></ListItem>
     <ListItem>支持测试 Vue、React、Lit 等框架中的组件。</ListItem>
-    <ListItem>开箱即用的 TypeScript / JSX 支持。</ListItem>
+    <ListItem>开箱即用的 TypeScript / JSX 支持</ListItem>
     <ListItem>ESM 优先 ， 顶级 await</ListItem>
     <ListItem>通过 <a href="https://github.com/Aslemammad/tinypool" target="_blank">tinypool</a> 使用 Worker 线程尽可能多地并发运行</ListItem>
     <ListItem>测试套件和测试的过滤、超时、并发</ListItem>
     <ListItem><a target="_blank" href="https://jestjs.io/zh-Hans/docs/snapshot-testing" rel="nofollow">Jest 的快照功能</a></ListItem>
-    <ListItem>内置 <a target="_blank" href="https://www.chaijs.com/" rel="nofollow">Chai</a> 进行断言 + 与<a href="https://jestjs.io/zh-Hans/docs/expect" rel="nofollow">Jest expect 语法</a>兼容的 API</ListItem>
+    <ListItem>内置 <a target="_blank" href="https://www.chaijs.com/" rel="nofollow">Chai</a> 进行断言 + 与 <a href="https://jestjs.io/zh-Hans/docs/expect" rel="nofollow">Jest expect 语法</a>兼容的 API</ListItem>
     <ListItem>内置用于模拟(mocking)的 <a target="_blank" href="https://github.com/Aslemammad/tinyspy" rel="nofollow">Tinyspy</a></ListItem>
     <ListItem>使用 <a target="_blank" href="https://github.com/jsdom/jsdom">jsdom</a> 或 <a target="_blank" href="https://github.com/capricorn86/happy-dom">happy-dom</a> 用于 DOM 模拟(DOM mocking)</ListItem>
     <ListItem>通过 <a target="_blank" href="https://github.com/bcoe/c8">c8</a> 来输出代码测试覆盖率</ListItem>
