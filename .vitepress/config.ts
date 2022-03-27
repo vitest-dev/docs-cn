333c5b5751a5e0f88a6d497294e162c567c37463
ryanmoyo <iherewithmyheart@gmail.com>
2022-03-27 21:28:48 +0800

    ✏️ fix typos: follow original repo - repo - title

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vitepress'
-import { version } from '../package.json'
+import { defineConfig } from "vitepress";
+import { version } from "../package.json";
 import {
   contributing,
   discord,
@@ -10,123 +10,145 @@ import {
   twitter,
   vitestDescription,
   vitestName,
-} from '../docs-data'
-import { coreTeamMembers } from '../src/contributors'
+} from "../docs-data";
+import { coreTeamMembers } from "../src/contributors";
 
 export default defineConfig({
   title: vitestName,
   description: vitestDescription,
   head: [
-    ['meta', { name: 'theme-color', content: '#ffffff' }],
-    ['link', { rel: 'icon', href: '/logo.svg', type: 'image/svg+xml' }],
-    ['link', { rel: 'alternate icon', href: '/favicon.ico', type: 'image/png', sizes: '16x16' }],
-    ['meta', { name: 'author', content: `${coreTeamMembers.map(c => c.name).join(', ')} and ${vitestName} contributors` }],
-    ['meta', { name: 'keywords', content: 'vitest, vite, test, coverage, snapshot, react, vue, preact, svelte, solid, lit, ruby, cypress, puppeteer, jsdom, happy-dom, test-runner, jest, typescript, esm, tinypool, tinyspy, c8, node' }],
-    ['meta', { property: 'og:title', content: vitestName }],
-    ['meta', { property: 'og:description', content: vitestDescription }],
-    ['meta', { property: 'og:url', content: ogUrl }],
-    ['meta', { property: 'og:image', content: ogImage }],
-    ['meta', { name: 'twitter:title', content: vitestName }],
-    ['meta', { name: 'twitter:description', content: vitestDescription }],
-    ['meta', { name: 'twitter:image', content: ogImage }],
-    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
-    ['link', { href: font, rel: 'stylesheet' }],
-    ['link', { rel: 'mask-icon', href: '/logo.svg', color: '#ffffff' }],
-    ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' }],
+    ["meta", { name: "theme-color", content: "#ffffff" }],
+    ["link", { rel: "icon", href: "/logo.svg", type: "image/svg+xml" }],
+    [
+      "link",
+      {
+        rel: "alternate icon",
+        href: "/favicon.ico",
+        type: "image/png",
+        sizes: "16x16",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "author",
+        content: `${coreTeamMembers
+          .map((c) => c.name)
+          .join(", ")} and ${vitestName} contributors`,
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "keywords",
+        content:
+          "vitest, vite, test, coverage, snapshot, react, vue, preact, svelte, solid, lit, ruby, cypress, puppeteer, jsdom, happy-dom, test-runner, jest, typescript, esm, tinypool, tinyspy, c8, node",
+      },
+    ],
+    ["meta", { property: "og:title", content: vitestName }],
+    ["meta", { property: "og:description", content: vitestDescription }],
+    ["meta", { property: "og:url", content: ogUrl }],
+    ["meta", { property: "og:image", content: ogImage }],
+    ["meta", { name: "twitter:title", content: vitestName }],
+    ["meta", { name: "twitter:description", content: vitestDescription }],
+    ["meta", { name: "twitter:image", content: ogImage }],
+    ["meta", { name: "twitter:card", content: "summary_large_image" }],
+    ["link", { href: font, rel: "stylesheet" }],
+    ["link", { rel: "mask-icon", href: "/logo.svg", color: "#ffffff" }],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        href: "/apple-touch-icon.png",
+        sizes: "180x180",
+      },
+    ],
   ],
   themeConfig: {
-    repo: 'vitest-dev/vitest',
-    logo: '/logo.svg',
-    docsDir: 'docs',
-    docsBranch: 'main',
+    repo: "vitest-dev/docs-cn",
+    logo: "/logo.svg",
+    docsDir: "docs",
+    docsBranch: "main",
     editLinks: true,
-    editLinkText: '建议更改此页面',
+    editLinkText: "建议更改此页面",
 
-    /**
     algolia: {
-      appId: 'ZTF29HGJ69',
-      apiKey: '9c3ced6fed60d2670bb36ab7e8bed8bc',
-      indexName: 'vitest',
+      appId: "ZTF29HGJ69",
+      apiKey: "9c3ced6fed60d2670bb36ab7e8bed8bc",
+      indexName: "vitest",
       // searchParameters: {
       //   facetFilters: ['tags:en']
       // }
     },
-    */
     nav: [
-      { text: '指引', link: '/guide/' },
-      { text: 'API', link: '/api/' },
-      { text: '配置', link: '/config/' },
+      { text: "指引", link: "/guide/" },
+      { text: "API", link: "/api/" },
+      { text: "配置", link: "/config/" },
       // { text: 'Plugins', link: '/plugins/' },
       {
         text: `v${version}`,
         items: [
           {
-            text: '版本发布 ',
+            text: "版本发布 ",
             link: releases,
           },
           {
-            text: '社区指南 ',
+            text: "社区指南 ",
             link: contributing,
           },
         ],
-
       },
       {
-        text: 'Discord 聊天室',
-        link: discord
+        text: "Discord",
+        link: discord,
       },
       {
-        text: 'Twitter',
-        link: twitter
+        text: "Twitter",
+        link: twitter,
       },
       {
-        text: '语言',
+        text: "简体中文",
         items: [
           {
-            text: 'English',
-            link: ogUrl
+            text: "English",
+            link: ogUrl,
           },
           {
-            text: '简体中文',
-            link: '/'
+            text: "简体中文",
+            link: "/",
           },
-          /**{
-            text: '日本語',
-            link: 'https://ja.vitest.dev'
-          } */
-        ]
-      }
+        ],
+      },
     ],
 
     sidebar: {
-      '/config/': 'auto',
-      '/api/': 'auto',
+      "/config/": "auto",
+      "/api/": "auto",
       // '/plugins': 'auto',
       // catch-all fallback
-      '/': [
+      "/": [
         {
-          text: '开始',
+          text: "开始",
           children: [
             {
-              text: '简介',
-              link: '/guide/why',
-
+              text: "简介",
+              link: "/guide/why",
             },
             {
-              text: '快速开始',
-              link: '/guide/'
+              text: "快速开始",
+              link: "/guide/",
             },
             {
-              text: '特性',
-              link: '/guide/features'
+              text: "特性",
+              link: "/guide/features",
             },
             {
-              text: '模拟',
-              link: '/guide/mocking'
+              text: "模拟",
+              link: "/guide/mocking",
             },
             {
-              text: '调试',
-              link: '/guide/debugging'
+              text: "调试",
+              link: "/guide/debugging",
             },
             /* TODO
             {
@@ -135,20 +157,20 @@ export default defineConfig({
             },
             */
             {
-              text: '源码内测试',
-              link: '/guide/in-source'
+              text: "源码内测试",
+              link: "/guide/in-source",
             },
             {
-              text: '比较',
-              link: '/guide/comparisons'
+              text: "比较",
+              link: "/guide/comparisons",
             },
             {
-              text: '迁移指南',
-              link: '/guide/migration'
-            }
-          ]
+              text: "迁移指南",
+              link: "/guide/migration",
+            },
+          ],
         },
       ],
     },
   },
-})
+});
