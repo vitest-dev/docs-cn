aab536d4022339e70ad9dbdf81754e57c0f08e89
Wáng Wěi Tāo <softweitao@126.com>
2023-02-01 06:40:55 +0000

    docs(cn): conflict resolution

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
 import { defineConfig } from "vitepress";
+import { withPwa } from "@vite-pwa/vitepress";
 import { version } from "../package.json";
-=======
-import { defineConfig } from 'vitepress'
-import { withPwa } from '@vite-pwa/vitepress'
-import { version } from '../../package.json'
->>>>>>> 39b585a80ccefc4e5a456ccf5f42968e72e063e6
+
 import {
   contributing,
   discord,
@@ -15,284 +11,261 @@ import {
   ogUrl,
   releases,
   twitter,
-<<<<<<< HEAD
   vitestDescription,
   vitestName,
 } from "./meta";
+import { pwa } from "./scripts/pwa";
+import { transformHead } from "./scripts/transformHead";
 import { teamMembers } from "./contributors";
 
-export default defineConfig({
-  lang: "en-US",
-  title: vitestName,
-  description: vitestDescription,
-  head: [
-    ["meta", { name: "theme-color", content: "#729b1a" }],
-    ["link", { rel: "icon", href: "/logo.svg", type: "image/svg+xml" }],
-    [
-      "link",
-      {
-        rel: "alternate icon",
-        href: "/favicon.ico",
-        type: "image/png",
-        sizes: "16x16",
-      },
-    ],
-    [
-      "meta",
-      {
-        name: "author",
-        content: `${teamMembers
-          .map((c) => c.name)
-          .join(", ")} and ${vitestName} contributors`,
-      },
-    ],
-    [
-      "meta",
-      {
-        name: "keywords",
-        content:
-          "vitest, vite, test, coverage, snapshot, react, vue, preact, svelte, solid, lit, ruby, cypress, puppeteer, jsdom, happy-dom, test-runner, jest, typescript, esm, tinypool, tinyspy, c8, node",
-      },
-    ],
-    ["meta", { property: "og:title", content: vitestName }],
-    ["meta", { property: "og:description", content: vitestDescription }],
-    ["meta", { property: "og:url", content: ogUrl }],
-    ["meta", { property: "og:image", content: ogImage }],
-    ["meta", { name: "twitter:title", content: vitestName }],
-    ["meta", { name: "twitter:description", content: vitestDescription }],
-    ["meta", { name: "twitter:image", content: ogImage }],
-    ["meta", { name: "twitter:card", content: "summary_large_image" }],
-    ["link", { href: font, rel: "stylesheet" }],
-    ["link", { rel: "mask-icon", href: "/logo.svg", color: "#ffffff" }],
-    [
-      "link",
-      {
-        rel: "apple-touch-icon",
-        href: "/apple-touch-icon.png",
-        sizes: "180x180",
-      },
-    ],
-=======
-  vitestDescription, vitestName,
-} from './meta'
-import { pwa } from './scripts/pwa'
-import { transformHead } from './scripts/transformHead'
-import { teamMembers } from './contributors'
-
-export default withPwa(defineConfig({
-  lang: 'en-US',
-  title: vitestName,
-  description: vitestDescription,
-  head: [
-    ['meta', { name: 'theme-color', content: '#729b1a' }],
-    ['link', { rel: 'icon', href: '/logo.svg', type: 'image/svg+xml' }],
-    ['link', { rel: 'alternate icon', href: '/favicon.ico', type: 'image/png', sizes: '16x16' }],
-    ['meta', { name: 'author', content: `${teamMembers.map(c => c.name).join(', ')} and ${vitestName} contributors` }],
-    ['meta', { name: 'keywords', content: 'vitest, vite, test, coverage, snapshot, react, vue, preact, svelte, solid, lit, ruby, cypress, puppeteer, jsdom, happy-dom, test-runner, jest, typescript, esm, tinypool, tinyspy, c8, node' }],
-    ['meta', { property: 'og:title', content: vitestName }],
-    ['meta', { property: 'og:description', content: vitestDescription }],
-    ['meta', { property: 'og:url', content: ogUrl }],
-    ['meta', { property: 'og:image', content: ogImage }],
-    ['meta', { name: 'twitter:title', content: vitestName }],
-    ['meta', { name: 'twitter:description', content: vitestDescription }],
-    ['meta', { name: 'twitter:image', content: ogImage }],
-    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
-    ['link', { rel: 'preload', as: 'style', onload: 'this.onload=null;this.rel=\'stylesheet\'', href: font }],
-    ['noscript', {}, `<link rel="stylesheet" crossorigin="anonymous" href="${font}" />`],
-    ['link', { rel: 'mask-icon', href: '/logo.svg', color: '#ffffff' }],
-    ['link', { rel: 'apple-touch-icon', href: '/apple-touch-icon.png', sizes: '180x180' }],
->>>>>>> 39b585a80ccefc4e5a456ccf5f42968e72e063e6
-  ],
-  lastUpdated: true,
-  markdown: {
-    theme: {
-      light: "vitesse-light",
-      dark: "vitesse-dark",
-    },
-  },
-  themeConfig: {
-    logo: "/logo.svg",
-
-    editLink: {
-      pattern: "https://github.com/vitest-dev/vitest/tree/main/docs/:path",
-      text: "为此页提供修改建议",
-    },
-
-    // algolia: {
-    //   appId: 'ZTF29HGJ69',
-    //   apiKey: '9c3ced6fed60d2670bb36ab7e8bed8bc',
-    //   indexName: 'vitest',
-    //   // searchParameters: {
-    //   //   facetFilters: ['tags:en'],
-    //   // },
-    // },
-
-    localeLinks: {
-      text: "简体中文",
-      items: [{ text: "English", link: "https://vitest.dev" }],
-    },
-
-    socialLinks: [
-      { icon: "twitter", link: twitter },
-      { icon: "discord", link: discord },
-      { icon: "github", link: github },
-    ],
-
-    footer: {
-      message: "Released under the MIT License.",
-      copyright:
-        "Copyright © 2021-PRESENT Anthony Fu, Matías Capeletto and Vitest contributors",
-    },
-
-    nav: [
-      { text: "指南", link: "/guide/" },
-      { text: "API", link: "/api/" },
-      { text: "配置", link: "/config/" },
-      {
-        text: `v${version}`,
-        items: [
-          {
-            text: "版本发布",
-            link: releases,
-          },
-          {
-            text: "社区指南",
-            link: contributing,
-          },
-        ],
-      },
-    ],
-
-    sidebar: {
-      // TODO: bring sidebar of apis and config back
-      "/": [
+export default withPwa(
+  defineConfig({
+    lang: "en-US",
+    title: vitestName,
+    description: vitestDescription,
+    head: [
+      ["meta", { name: "theme-color", content: "#729b1a" }],
+      ["link", { rel: "icon", href: "/logo.svg", type: "image/svg+xml" }],
+      [
+        "link",
         {
-          text: "指南",
-          items: [
-            {
-              text: "简介",
-              link: "/guide/why",
-            },
-            {
-              text: "快速起步",
-              link: "/guide/",
-            },
-            {
-              text: "主要功能",
-              link: "/guide/features",
-            },
-            {
-              text: "命令行界面",
-              link: "/guide/cli",
-            },
-            {
-              text: "测试筛选",
-              link: "/guide/filtering",
-            },
-            {
-              text: "测试覆盖率",
-              link: "/guide/coverage",
-            },
-            {
-              text: "测试快照",
-              link: "/guide/snapshot",
-            },
-            {
-              text: "模拟对象",
-              link: "/guide/mocking",
-            },
-            {
-              text: "类型测试",
-              link: "/guide/testing-types",
-            },
-            {
-              text: "Vitest UI",
-              link: "/guide/ui",
-            },
-            {
-              text: "源码内联测试",
-              link: "/guide/in-source",
-            },
-            {
-              text: "测试上下文",
-              link: "/guide/test-context",
-            },
-            {
-              text: "测试环境",
-              link: "/guide/environment",
-            },
-            {
-              text: "扩展匹配器",
-              link: "/guide/extending-matchers",
-            },
-            {
-              text: "IDE 插件",
-              link: "/guide/ide",
-            },
-            {
-              text: "调试",
-              link: "/guide/debugging",
-            },
-            {
-              text: "与其他测试框架对比",
-              link: "/guide/comparisons",
-            },
-            {
-              text: "迁移指南",
-              link: "/guide/migration",
-            },
-          ],
+          rel: "alternate icon",
+          href: "/favicon.ico",
+          type: "image/png",
+          sizes: "16x16",
         },
+      ],
+      [
+        "meta",
         {
-          text: "API",
-          items: [
-            {
-<<<<<<< HEAD
-              text: "API 索引",
-              link: "/api/",
-=======
-              text: 'Test API Reference',
-              link: '/api/',
->>>>>>> 39b585a80ccefc4e5a456ccf5f42968e72e063e6
-            },
-            {
-              text: 'Mock Functions',
-              link: '/api/mock',
-            },
-            {
-              text: 'Vi Utility',
-              link: '/api/vi',
-            },
-            {
-              text: 'Expect',
-              link: '/api/expect',
-            },
-            {
-              text: 'ExpectTypeOf',
-              link: '/api/expect-typeof',
-            },
-            {
-              text: 'assertType',
-              link: '/api/assert-type',
-            },
-          ],
+          name: "author",
+          content: `${teamMembers
+            .map((c) => c.name)
+            .join(", ")} and ${vitestName} contributors`,
         },
+      ],
+      [
+        "meta",
         {
-          text: "配置",
+          name: "keywords",
+          content:
+            "vitest, vite, test, coverage, snapshot, react, vue, preact, svelte, solid, lit, ruby, cypress, puppeteer, jsdom, happy-dom, test-runner, jest, typescript, esm, tinypool, tinyspy, c8, node",
+        },
+      ],
+      ["meta", { property: "og:title", content: vitestName }],
+      ["meta", { property: "og:description", content: vitestDescription }],
+      ["meta", { property: "og:url", content: ogUrl }],
+      ["meta", { property: "og:image", content: ogImage }],
+      ["meta", { name: "twitter:title", content: vitestName }],
+      ["meta", { name: "twitter:description", content: vitestDescription }],
+      ["meta", { name: "twitter:image", content: ogImage }],
+      ["meta", { name: "twitter:card", content: "summary_large_image" }],
+      [
+        "link",
+        {
+          rel: "preload",
+          as: "style",
+          onload: "this.onload=null;this.rel='stylesheet'",
+          href: font,
+        },
+      ],
+      [
+        "noscript",
+        {},
+        `<link rel="stylesheet" crossorigin="anonymous" href="${font}" />`,
+      ],
+      ["link", { rel: "mask-icon", href: "/logo.svg", color: "#ffffff" }],
+      [
+        "link",
+        {
+          rel: "apple-touch-icon",
+          href: "/apple-touch-icon.png",
+          sizes: "180x180",
+        },
+      ],
+    ],
+    lastUpdated: true,
+    markdown: {
+      theme: {
+        light: "vitesse-light",
+        dark: "vitesse-dark",
+      },
+    },
+    themeConfig: {
+      logo: "/logo.svg",
+
+      editLink: {
+        pattern: "https://github.com/vitest-dev/vitest/tree/main/docs/:path",
+        text: "为此页提供修改建议",
+      },
+
+      // algolia: {
+      //   appId: 'ZTF29HGJ69',
+      //   apiKey: '9c3ced6fed60d2670bb36ab7e8bed8bc',
+      //   indexName: 'vitest',
+      //   // searchParameters: {
+      //   //   facetFilters: ['tags:en'],
+      //   // },
+      // },
+
+      localeLinks: {
+        text: "简体中文",
+        items: [{ text: "English", link: "https://vitest.dev" }],
+      },
+
+      socialLinks: [
+        { icon: "twitter", link: twitter },
+        { icon: "discord", link: discord },
+        { icon: "github", link: github },
+      ],
+
+      footer: {
+        message: "Released under the MIT License.",
+        copyright:
+          "Copyright © 2021-PRESENT Anthony Fu, Matías Capeletto and Vitest contributors",
+      },
+
+      nav: [
+        { text: "指南", link: "/guide/" },
+        { text: "API", link: "/api/" },
+        { text: "配置", link: "/config/" },
+        {
+          text: `v${version}`,
           items: [
             {
-              text: "配置索引",
-              link: "/config/",
+              text: "版本发布",
+              link: releases,
+            },
+            {
+              text: "社区指南",
+              link: contributing,
             },
           ],
         },
       ],
+
+      sidebar: {
+        // TODO: bring sidebar of apis and config back
+        "/": [
+          {
+            text: "指南",
+            items: [
+              {
+                text: "简介",
+                link: "/guide/why",
+              },
+              {
+                text: "快速起步",
+                link: "/guide/",
+              },
+              {
+                text: "主要功能",
+                link: "/guide/features",
+              },
+              {
+                text: "命令行界面",
+                link: "/guide/cli",
+              },
+              {
+                text: "测试筛选",
+                link: "/guide/filtering",
+              },
+              {
+                text: "测试覆盖率",
+                link: "/guide/coverage",
+              },
+              {
+                text: "测试快照",
+                link: "/guide/snapshot",
+              },
+              {
+                text: "模拟对象",
+                link: "/guide/mocking",
+              },
+              {
+                text: "类型测试",
+                link: "/guide/testing-types",
+              },
+              {
+                text: "Vitest UI",
+                link: "/guide/ui",
+              },
+              {
+                text: "源码内联测试",
+                link: "/guide/in-source",
+              },
+              {
+                text: "测试上下文",
+                link: "/guide/test-context",
+              },
+              {
+                text: "测试环境",
+                link: "/guide/environment",
+              },
+              {
+                text: "扩展匹配器",
+                link: "/guide/extending-matchers",
+              },
+              {
+                text: "IDE 插件",
+                link: "/guide/ide",
+              },
+              {
+                text: "调试",
+                link: "/guide/debugging",
+              },
+              {
+                text: "与其他测试框架对比",
+                link: "/guide/comparisons",
+              },
+              {
+                text: "迁移指南",
+                link: "/guide/migration",
+              },
+            ],
+          },
+          {
+            text: "API",
+            items: [
+              {
+                text: "API 索引",
+                link: "/api/",
+              },
+              {
+                text: "Mock Functions",
+                link: "/api/mock",
+              },
+              {
+                text: "Vi Utility",
+                link: "/api/vi",
+              },
+              {
+                text: "Expect",
+                link: "/api/expect",
+              },
+              {
+                text: "ExpectTypeOf",
+                link: "/api/expect-typeof",
+              },
+              {
+                text: "assertType",
+                link: "/api/assert-type",
+              },
+            ],
+          },
+          {
+            text: "配置",
+            items: [
+              {
+                text: "配置索引",
+                link: "/config/",
+              },
+            ],
+          },
+        ],
+      },
     },
-  },
-<<<<<<< HEAD
-});
-=======
-  pwa,
-  transformHead,
-}))
->>>>>>> 39b585a80ccefc4e5a456ccf5f42968e72e063e6
+    pwa,
+    transformHead,
+  })
+);
