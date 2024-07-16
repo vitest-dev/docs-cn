e9e295b4c6420e725c55b851626c291517a9cd8a
softweitao_37264 <softweitao@126.com>
2024-07-16 18:00:18 +0800

    chore: fix build error in netlify

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,11 @@
-// eslint.config.js
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  // Enable stylistic formatting rules
   stylistic: true,
-
-  // Or customize the stylistic rules
-  stylistic: {
-    indent: 2, // 4, or 'tab'
-    quotes: 'single', // or 'double'
-  },
-
-  // TypeScript and Vue are auto-detected, you can also explicitly enable them:
   typescript: true,
   vue: true,
-
-  // Disable jsonc and yaml support
   jsonc: false,
   yaml: false,
-
-  // `.eslintignore` is no longer supported in Flat config, use `ignores` instead
   ignores: [
     'dist',
     'node_modules',
@@ -29,4 +15,8 @@ export default antfu({
     'coverage',
     '!.vitepress',
   ],
+  rules: {
+    'no-restricted-globals': 'off',
+    'no-empty-pattern': 'off',
+  },
 })
