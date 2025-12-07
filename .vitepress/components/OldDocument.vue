e21d549db1fad8c4b40c569dee441cb69d436dba
noise <noisefan@163.com>
2025-12-07 18:44:20 +0800

    docs: add "old documentation" banner

---
+++ .vitepress/components/OldDocument.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="old-document">
+    <p>
+      本文档适用于 v2 <strong>（旧版本）</strong>。如需查看最新版本请访问
+      <a href="https://cn.vitest.dev" class="new-document-link">https://cn.vitest.dev</a>.
+    </p>
+  </div>
+</template>
+
+<style>
+:root {
+  --vp-layout-top-height: 96px;
+  @media (min-width: 455px) {
+    --vp-layout-top-height: 64px;
+  }
+  @media (min-width: 960px) {
+    --vp-layout-top-height: 32px;
+  }
+}
+
+.old-document {
+  position: fixed;
+  display: flex;
+  height: var(--vp-layout-top-height);
+  width: 100%;
+  padding: 4px 32px;
+  justify-content: center;
+  align-items: center;
+  color: var(--vp-c-old-document-text);
+  background: var(--vp-c-old-document-bg);
+  z-index: var(--vp-z-index-layout-top);
+
+  .new-document-link {
+    text-decoration: underline;
+    color: var(--vp-c-text-1);
+    &:hover {
+      color: var(--vp-c-text-2);
+    }
+  }
+}
+</style>
